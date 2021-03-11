8deb8d8dbb60c6f662bcd9d80c5e486e8a6e69dc
KuldeepSingh3011 <ks517633@gmail.com>
2021-03-11 18:45:36 +0530

    yz

---
+++ yz/abcd.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define mod 1000000007
+#define pb(a) push_back(a)
+#define pi pair<int,int>
+#define f first
+#define s second
+#define vi vector<ll> 
+#define vc vector<char>
+#define endl "\n"
+void solve()
+{
+  ll n;
+  cin>>n;
+  ll arr[n];
+  for(int i=0;i<n;i++)
+  	cin>>arr[i];
+  
+  
+}
+int main()
+{
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+    ios::sync_with_stdio(0); cin.tie(0);
+	int t;
+	cin>>t;
+	while(t--)
+	solve();
+ return 0;	
+}

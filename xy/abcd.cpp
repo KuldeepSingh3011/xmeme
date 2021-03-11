48f7620491f21884d1724dfa76b8231279a66276
KuldeepSingh3011 <ks517633@gmail.com>
2021-03-11 18:40:35 +0530

    xy folder added

---
+++ xy/abcd.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define mod 1000000007
+#define pb(a) push_back(a)
+#define pi pair<int,int>
+#define f first
+#define s second
+#define vi vector<ll> 
+#define vc vector<char>
+#define endl "\n"
+void solve()
+{
+  
+}
+int main()
+{
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+    ios::sync_with_stdio(0); cin.tie(0);
+	int t;
+	cin>>t;
+	while(t--)
+	solve();
+ return 0;	
+}
